339782d8dfee210d806da923814326aa9a659783
victorivus <18736740+Victorivus@users.noreply.github.com>
2024-09-19 11:13:03 +0200

    bug loosing data on refresh

--- peaks.js
+++ peaks.js
@@ -1,4 +1,6 @@
 let peaks;
+// Load saved data from localStorage if available
+peaks = JSON.parse(localStorage.getItem("peaks"));
 if (!peaks) {
     fetch('3000_peaks_pyrenees.json')
     .then(response => response.json())
